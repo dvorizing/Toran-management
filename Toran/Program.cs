7efaad7085b425a367ef61244254fb13f15dba94
dvorizing <dvori177359@gmail.com>
2025-06-22 08:05:55 +0300

    AddCors to program

--- Toran/Program.cs
+++ Toran/Program.cs
@@ -22,6 +22,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
